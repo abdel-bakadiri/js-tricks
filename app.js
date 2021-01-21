72f36ff9904a0913cfe5c48b6a55d47723e246c9
abdel-bakadiri <abdelouahab.bakadiri@gmail.com>
2021-01-22 00:47:44 +0100

    feat(): some tricks for JS

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+const { performance } = require("perf_hooks");
+//  Unique object
+let sart = performance.now();
 const array = [1, 2, 3, 4, 5, 1, 3, 2, 5, 3, 4];
 const uniqueArray = Array.from(new Set(array));
 console.log(uniqueArray);
+// transform an array to an object
+const array1 = ["fofo", "toto", "fifi", "titi", "mimi"];
+const obj = { ...array1 };
+console.log(obj);
+// transform obj to an array
+const obj1 = {
+  name: "abdel",
+  score: "48",
+  level: "intermediate",
+};
+const arrayFormObjUsingKeys = Object.keys(obj1);
+console.log(arrayFormObjUsingKeys);
+const arrayFormObjUsingValues = Object.values(obj1);
+console.log(arrayFormObjUsingValues);
+let end = performance.now();
+console.log(`${end - sart} MS`);
--- todo.md
+++ todo.md
@@ -1,3 +1,11 @@
 ## Trick 1:
 
-- Filter a unique element in an array
+Filter a unique element in an array
+
+## Trick 2:
+
+transform an object to an array
+
+## trick 3:
+
+transform an object to an array
